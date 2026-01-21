e1df32afdf5f47b9395fe62fc39e650c41ea4af5
suhs <ranga,kg@gmail.co>
2026-01-21 23:06:16 +0530

    Updated Booking Page

--- arify-frontend/src/App.jsx
+++ arify-frontend/src/App.jsx
@@ -4,8 +4,12 @@ import Book from "./booking/Book";
 export default function App() {
   return (
     <Routes>
+      {/* NORMAL */}
       <Route path="/" element={<Navigate to="/book" replace />} />
       <Route path="/book" element={<Book />} />
+
+      {/* 🔥 RENDER STATIC FIX */}
+      <Route path="/index.html" element={<Book />} />
     </Routes>
   );
 }
